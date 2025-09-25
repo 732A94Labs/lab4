2e0aeeb660ea1b43118a616a7d217ad5b26c08c4
Nils <nils.fahrni@students.fhnw.ch>
2025-09-25 09:50:31 +0200

    added theme

---
+++ R/liu_theme.R
@@ -0,0 +1,11 @@
+#' Linköping University ggplot2 theme
+#' @return A ggplot2 theme object
+#' @export
+liu_theme <- function() {
+  ggplot2::theme_minimal(base_size = 11) +
+    ggplot2::theme(
+      plot.background = ggplot2::element_rect(fill = "#d1f4f6"),
+      plot.title = ggplot2::element_text(face = "bold"),
+      panel.grid.minor = ggplot2::element_blank()
+    )
+}
